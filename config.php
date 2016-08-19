bbcd2b6f344878109cf3bb1808c8ca43a4ac0d40
ardubovik <ar_dubovik@mail.ru>
2016-08-20 00:26:30 +0700

    add config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define('SITE_ROOT', __DIR__);
+define('WWW_ROOT', SITE_ROOT . '/www');
+define('DATA_DIR', SITE_ROOT . '/data');
+define('LIB_DIR', SITE_ROOT . '/lib');
+define('TPL_DIR', SITE_ROOT . '/templates');
+
+include_once(LIB_DIR . '/functions.php');
+
+?>
